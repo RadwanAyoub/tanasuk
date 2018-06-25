52e8e9460a70eda4babf76c6281c1efb63708c13
rayoub95 <rayoub@tanasuk.com>
2018-06-25 13:42:46 +0300

    Add unicorn configuration and yml files

---
+++ Tanasuk/Pipelines/UploadeLimitation.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Form.Core.Pipelines.FormUploadFile;
+namespace Tanasuk.Pipelines
+{
+    public class UploadeLimitation
+    {
+        public void Process(FormUploadFileArgs args)
+        {
+            int size = 10485760; // == 10 Mb
+            if (args.File.Data.Length > size)
+            {
+                Sitecore.Diagnostics.Log.Info(string.Format("User {0} tried to upload a file larger than 10 Mb. The file name is {1}", Sitecore.Context.User.Name, args.File.FileName), this);
+                args.AbortPipeline();
+            }
+        }
+    }
+}
---
+++ Tanasuk/SaveActions/ContactUsLog.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.Security.Authentication;
+using Sitecore.WFFM.Abstractions.Actions;
+
+namespace Tanasuk.SaveActions
+{
+    public class ContactUsLog : ISaveAction
+    {
+        public ID ActionID { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string UniqueKey { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public ActionType ActionType => throw new System.NotImplementedException();
+        public void Execute(ID formId, AdaptedResultList adaptedFields, ActionCallContext actionCallContext = null, params object[] data)
+        {
+            throw new System.NotImplementedException();
+        }
+        public ActionState QueryState(ActionQueryContext queryContext)
+        {
+            throw new System.NotImplementedException();
+        }
+        public void Log()
+        {
+            Sitecore.Diagnostics.Log.Info("XXX________________________________XXX", this);
+        }
+    }
+}
